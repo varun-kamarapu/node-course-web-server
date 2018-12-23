b6730b5a1286a28a0a86da1b5aa4b6b6af1c61fa
unknown <varun.kamarapu@gmail.com>
2018-12-23 15:08:20 +0000

    modified files for heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+var port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -70,6 +71,6 @@ app.get('/ask', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('The server is up on port 3000');
+app.listen(port, () => {
+  console.log(`The server is up on port ${port}`);
 });
